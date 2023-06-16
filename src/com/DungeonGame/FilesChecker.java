37d9428205c2c61641601968cefd6acd3c95a802
POPPhil <101636745+POPPhil@users.noreply.github.com>
2023-06-16 22:07:37 +0200

    Checking all class

--- src/com/DungeonGame/FilesChecker.java
+++ src/com/DungeonGame/FilesChecker.java
@@ -1,17 +1,35 @@
 package com.DungeonGame;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class FilesChecker {
 
     /**
-     * Verifie si Constants.java est existant
+     * Vérifie si les fichiers de classes .java sont existants
      */
-    static boolean checkConstantsFile() {
-        try {
-            Class.forName("com.DungeonGame.Constants");
-            return true;
-        } catch (ClassNotFoundException e) {
-            System.err.println("Erreur : La classe Constants est manquante.");
-            return false;
+    static boolean checkClassFiles() {
+        String packageDirectory = "/com/DungeonGame";
+
+        String[] classNames = {"Constants", "FileReader", "Game", "GameStatus", "MessageManager", "MonsterManager", "PlayerManager", "Printer"};    // Liste des package a verifier
+
+        for (String className : classNames) {
+            String classFilePath = packageDirectory + "/" + className + ".class";
+            InputStream inputStream = FilesChecker.class.getResourceAsStream(classFilePath);
+            if (inputStream == null) {
+                System.err.println("\n\nErreur : La classe '" + className + "' est manquante.\n\nLe programme ne peut pas s'exécuter !\n\n");
+                return false;
+            } else {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
+
+        return true;
     }
 }
